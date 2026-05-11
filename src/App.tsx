4f4c1192cb5dbb336562e294c32540b3a8a7997f
globalop <Amitkmishra0641@gmail.com>
2026-05-11 18:36:35 +0530

    UI: Hide maintenance banner (System is fully updated)

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
             <Sonner />
             <BrowserRouter>
               <Navbar />
-              <MaintenanceBanner />
+              {/* <MaintenanceBanner /> */}
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/scan" element={<ScanPage />} />
